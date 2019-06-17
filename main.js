60e225408c21485774880fa9eb2addf979d4aa0b
krystal gates <krystal_sabrina@yahoo.com>
2019-06-17 14:55:41 -0500

    initial HTML and exercise instructions

---
+++ main.js
@@ -0,0 +1,6 @@
+// Practice: Doctors
+// Lightning Exercise 1: Write a factory function that creates an object that represents a doctor. The function should accept three arguments.
+
+// Doctor's name
+// Specialty (Oncologist, pediatrician, etc...)
+// Address of practice
